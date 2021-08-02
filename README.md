3ceb43687b3bf547212740b206aea9e9dc6dafaa
Bebel-O-Rama <bebel-o-rama@hotmail.com>
2021-08-01 20:27:48 -0400

    Added a first setup for the code. We can read 1 or 2 images using their path, will update the README with the execution steps soon

--- README.md
+++ README.md
@@ -1,17 +1,29 @@
-# IMN269-DevoirFinal
+# Devoir Final - IMN269
+Nicolas Auclair-Labbé - aucn2303 \
+Ala Antabli - anta2801
 
-Calibration information
+Ce document contient les informations relatives à l'exécution du programme et à la calibration des caméras stéréo 
 
-1. Intrinsic parameters:
-- Pixel size: 4860 m x 3660 m
-- Radial distortion: zero
-- Lens diameter: 1/3 inch
-- Focal length: 12mm (to be checked with others)
+## Architectures et versions
+- OS: Windows 10
+- Python: Version 3.8.8
+- Numpy: 1.21.1
+- Opencv-Python: 4.5.3.56
+- Matplotlib: 3.4.2
 
-2. Extrinsic parameters:
-- Distance between the two cameras: 60 mm
-- Angle between the two cameras: zero
-- Distance between the two cameras and the face: 480 mm
+## Exécution du programme
+Ajouter ici les étapes d'exécution, donc les paramètres à entrer lorsqu'on veut exécuter le programme (et autre)
 
+## Informations de calibration
+1. Paramètres intrinsèques:
+- Taille du pixel: 4860m * 3660m
+- Distortion radiale: zero
+- Diamètre de la lentille: 1/3 pouce ou 8.47mm
+- Distance focale: 12mm
 
-More informations about the camera can be found here : https://www.aliexpress.com/item/32879864272.html
+2. Paramètres extrinsèques:
+- Distance entre les deux caméras: 60mm
+- Angle entre les deux caméras: nil
+- Distance entre le centre des deux caméras et le visage: 480mm
+
+Plus d'information à propos de la caméra peut être trouvé ici : https://www.aliexpress.com/item/32879864272.html
---
+++ stereo_cam_app.py
@@ -0,0 +1,73 @@
+# import collections
+import cv2
+import numpy as np
+import sys
+from typing import NamedTuple
+import os.path
+
+# Basic data structure to keep both images from a stereo capture
+class StereoImg(NamedTuple):
+    left_img: np.ndarray
+    right_img: np.ndarray
+
+# Get the StereoImg using the path passed as parameters
+def get_stereo_img(argv, img_type):
+    # We start by checking if the paths are pointing to some valid files.
+    if not os.path.isfile(argv[0]):
+        print("The path : ", argv[0], " doesn't point to a valid file.")
+        sys.exit()
+    elif len(argv) == 2 and not os.path.isfile(argv[0]):
+        print("The path : ", argv[1], " doesn't point to a valid file.")
+        sys.exit()
+
+    # If there's only one path, we assume both images are together and we split them in a left and right image.
+    if len(argv) == 1:
+        merged_img = cv2.imread(argv[0], img_type)
+        # Checks if the image has been read correctly
+        if merged_img is None:
+            print("Something went wrong and the image at the path : ", argv[0], " couldn't be read.")
+            sys.exit()
+        merged_h, merged_w = merged_img.shape
+
+        width_cutoff = merged_w // 2
+        img_left = merged_img[:, :width_cutoff]
+        img_right = merged_img[:, width_cutoff:]
+
+    # Otherwise, we just have to read both images
+    else:
+        img_left = cv2.imread(argv[0], img_type)
+        img_right = cv2.imread(argv[1], img_type)
+        # Checks if both images have been read correctly
+        if img_left is None:
+            print("Something went wrong and the image at the path : ", argv[0], " couldn't be read.")
+            sys.exit()
+        elif img_right is None:
+            print("Something went wrong and the image at the path : ", argv[1], " couldn't be read.")
+            sys.exit()
+
+    # Returns the combination the left and right image in a StereoImg
+    images = StereoImg(img_left, img_right)
+    return images
+
+
+def main(argv):
+    # If the number of parameters is > 2, exit the process
+    if len(argv) > 2 or len(argv) == 0:
+        print("The number of arguments should be either 1 or 2, please refer to the README.md for more information about the execution of this program.")
+        sys.exit()
+
+    # Get the images from the path passed as parametersf
+    # Since we want to match the stereo capture, we are setting the capture type to greyscale. It can be modified by changing the second parameter of the function
+    stereo_img = get_stereo_img(argv, 0)
+
+    # That's only there for debugging purpose, it shows the image read from the files
+    cv2.imshow('left', stereo_img.left_img)
+    cv2.imshow('right', stereo_img.right_img)
+
+
+    # Stop the program until we press a key and then closes every window that has been opened
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
